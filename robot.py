8fc0388faa40cd39109d5ec8ae05e2c3fa864085
Luke Carmichael <lcarmich@umich.edu>
2024-12-26 12:05:40 -0500

    rename

--- robot.py
+++ robot.py
@@ -1,113 +1,60 @@
 #!/usr/bin/env python3
-#
-# Copyright (c) FIRST and other WPILib contributors.
-# Open Source Software; you can modify and/or share it under the terms of
-# the WPILib BSD license file in the root directory of this project.
-#
 
-from rev import SparkMax
-
-#import commands2
-import wpilib
-
-#from robotcontainer import RobotContainer
 import time
+import wpilib
+import wpilib.drive
+import rev
+
+import phoenix6
 
 
 class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
-        # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-        # autonomous chooser on the dashboard.
-        # self.container = RobotContainer()
-        self.manual_test_init()
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.controller = wpilib.XboxController(0)
 
-    def manual_test_init(self):
-        self.driverController = wpilib.XboxController(0)
-        self.autonomousCommand = None
-        self.current_motor_index = 0
-        self.last_time_pressed = time.time()
-        # SPARK MAX CAN IDs
-        self.ids = [
-        ('kFrontLeftDrivingCanId', 10),
-        ('kFrontLeftTurningCanId', 11),
 
-        ('kRearLeftTurningCanId', 15),
-        ('kRearLeftDrivingCanId', 16),
+        driveMotorChannel = 11
+        self.drive11 = rev.SparkMax(driveMotorChannel, rev.SparkMax.MotorType.kBrushless)
 
-        ('kFrontRightTurningCanId', 20),
-        ('kFrontRightDrivingCanId', 21),
+        # We need to invert one side of the drivetrain so that positive voltages
+        # result in both sides moving forward. Depending on how your robot's
+        # gearbox is constructed, you might have to invert the left side instead.
+        #self.rightDrive.setInverted(True)
 
-        ('kRearRightTurningCanId', 25),
-        ('kRearRightDrivingCanId', 26),
-        ]
+        #self.cancoder12 = p6.hardware.CANcoder(12)
 
-        self.motors = {}
-        for lbl, cid in self.ids:
-            self.motors[cid] = SparkMax(cid, SparkMax.MotorType.kBrushless)
-
-        self.motor_label, self.can_id = self.ids[self.current_motor_index]
-        self.motor = self.motors[self.can_id]
-
-        self.on = False
-
-    def autonomousInit(self) -> None:
+    def teleopInit(self):
+        """This function is called once each time the robot enters teleoperated mode."""
         pass
-        #self.autonomousCommand = self.container.getAutonomousCommand()
 
-        #if self.autonomousCommand:
-        #    self.autonomousCommand.schedule()
+    def teleopPeriodic(self):
+        """This function is called periodically during teleoperated mode."""
 
-    def teleopInit(self) -> None:
+        #val = self.controller.getLeftX()
+        #self.drive.set(abs(val))
+        #abs_pos  = self.cancoder12.get_absolute_position()
+        #abs_pos.value
+
+        e = (int(time.time()) % 2) == 0
+        if e:
+            print('hello')
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
         pass
-        #if self.autonomousCommand:
-        #    self.autonomousCommand.cancel()
 
-    def teleopPeriodic(self) -> None:
-        # Teleop periodic logic
-        #self.driveWithJoystick(True)
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
         pass
-        self.tel_manual()
 
-    def tel_manual(self):
-        dc = self.driverController
-        now = time.time()
-        elapsed = now-self.last_time_pressed
-        print('elapsed:', elapsed)
+    def testInit(self):
+        """This function is called once each time the robot enters test mode."""
+        pass
 
-        if dc.getBButton() and elapsed >= 1:
-            self.current_motor_index += 1
-            if self.current_motor_index >= len(self.ids):
-                self.current_motor_index = 0
-            self.last_time_pressed = now
-            self.motor_label, self.can_id = self.ids[self.current_motor_index]
-            self.motor = self.motors[self.can_id]
-
-        if dc.getYButton() and elapsed >= 1:
-            self.current_motor_index -= 1
-            if self.current_motor_index <= -1:
-                self.current_motor_index = len(self.ids) - 1
-            self.last_time_pressed = now
-            self.motor_label, self.can_id = self.ids[self.current_motor_index]
-            self.motor = self.motors[self.can_id]
-
-        print(self.motor_label, self.can_id, self.motor)
-
-        if dc.getXButton(): 
-            self.motor.set(.5)
-        else:
-            self.motor.set(0)
-
-        # TODO encoder
-        # import phoenix6 as p6 
-        # x = p6.hardware.CANcoder(12)
-        # p2 = x.get_absolute_position()
-        # p2.value # 0-1 value?
-
-    
-    def testInit(self) -> None:
-        commands2.CommandScheduler.getInstance().cancelAll()
-
-
-if __name__ == "__main__":
-    wpilib.run(MyRobot)
-
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        pass
---
+++ robotcycle.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+from rev import SparkMax
+
+#import commands2
+import wpilib
+
+#from robotcontainer import RobotContainer
+import time
+
+
+class MyRobot(wpilib.TimedRobot):
+    def robotInit(self):
+        # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        # autonomous chooser on the dashboard.
+        # self.container = RobotContainer()
+        self.manual_test_init()
+
+    def manual_test_init(self):
+        self.driverController = wpilib.XboxController(0)
+        self.autonomousCommand = None
+        self.current_motor_index = 0
+        self.last_time_pressed = time.time()
+        # SPARK MAX CAN IDs
+        self.ids = [
+        ('kFrontLeftDrivingCanId', 10),
+        ('kFrontLeftTurningCanId', 11),
+
+        ('kRearLeftTurningCanId', 15),
+        ('kRearLeftDrivingCanId', 16),
+
+        ('kFrontRightTurningCanId', 20),
+        ('kFrontRightDrivingCanId', 21),
+
+        ('kRearRightTurningCanId', 25),
+        ('kRearRightDrivingCanId', 26),
+        ]
+
+        self.motors = {}
+        for lbl, cid in self.ids:
+            self.motors[cid] = SparkMax(cid, SparkMax.MotorType.kBrushless)
+
+        self.motor_label, self.can_id = self.ids[self.current_motor_index]
+        self.motor = self.motors[self.can_id]
+
+        self.on = False
+
+    def autonomousInit(self) -> None:
+        pass
+        #self.autonomousCommand = self.container.getAutonomousCommand()
+
+        #if self.autonomousCommand:
+        #    self.autonomousCommand.schedule()
+
+    def teleopInit(self) -> None:
+        pass
+        #if self.autonomousCommand:
+        #    self.autonomousCommand.cancel()
+
+    def teleopPeriodic(self) -> None:
+        # Teleop periodic logic
+        #self.driveWithJoystick(True)
+        pass
+        self.tel_manual()
+
+    def tel_manual(self):
+        dc = self.driverController
+        now = time.time()
+        elapsed = now-self.last_time_pressed
+        print('elapsed:', elapsed)
+
+        if dc.getBButton() and elapsed >= 1:
+            self.current_motor_index += 1
+            if self.current_motor_index >= len(self.ids):
+                self.current_motor_index = 0
+            self.last_time_pressed = now
+            self.motor_label, self.can_id = self.ids[self.current_motor_index]
+            self.motor = self.motors[self.can_id]
+
+        if dc.getYButton() and elapsed >= 1:
+            self.current_motor_index -= 1
+            if self.current_motor_index <= -1:
+                self.current_motor_index = len(self.ids) - 1
+            self.last_time_pressed = now
+            self.motor_label, self.can_id = self.ids[self.current_motor_index]
+            self.motor = self.motors[self.can_id]
+
+        print(self.motor_label, self.can_id, self.motor)
+
+        if dc.getXButton(): 
+            self.motor.set(.5)
+        else:
+            self.motor.set(0)
+
+        # TODO encoder
+        # import phoenix6 as p6 
+        # x = p6.hardware.CANcoder(12)
+        # p2 = x.get_absolute_position()
+        # p2.value # 0-1 value?
+
+    
+    def testInit(self) -> None:
+        commands2.CommandScheduler.getInstance().cancelAll()
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
+
